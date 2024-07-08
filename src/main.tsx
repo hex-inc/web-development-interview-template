afb46e51f5ec38ef673665628fb0c1be9f2cb81f
jkillian <jkillian@hex.tech>
2024-07-08 15:17:45 -0400

    - Fix warning about using `createRoot` on `document.body` - Change favicon link so it doesn't try to load a missing image

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.body).render(
+ReactDOM.createRoot(document.querySelector('#app')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
